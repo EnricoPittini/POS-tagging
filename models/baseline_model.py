3f36c02378b95c68f5802557b3ff140797a13124
Enrico Pittini <pittinienrico@hotmail.it>
2022-11-27 21:44:40 +0100

    Update baseline_model.py

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -1,17 +1,38 @@
 import tensorflow.keras as ks
 
-def build_baseline_model(sequence_length, n_classes, embedding_dim=50, latent_dim=128, embedding_matrix=None):
+def build_baseline_model(n_classes, embedding_dim=50, embedding_matrix=None, latent_dim=128):
+    """Build the baseline model, consisting in a BiLSTM plus a Dense layer on top.
 
-    # TODO remove sequence_length
-    inputs = ks.layers.Input(shape=(sequence_length,))
+    Parameters
+    ----------
+    n_classes : int
+        Number of classes (i.e. number of POS tags)
+    embedding_dim : int, optional
+        Dimensionality of the embeddings, by default 50
+    embedding_matrix : np.array, optional
+        Bidimensional matrix containing the words embeddings, by default None
+    latent_dim : int, optional
+        Dimensionality of the hidden states returned by each LSTM, by default 128.
+        Since the two LSTM are concatenated, the actual hidden states returned by the BiLSTM have dimensionality 2*`latent_dim`.    
 
-    V = embedding_matrix.shape[0]
-    embeddings =   ks.layers.Embedding(output_dim=embedding_dim, input_dim=V, input_length=sequence_length, 
+    Returns
+    -------
+    ks.Model
+        The model.
+    """
+    inputs = ks.layers.Input(shape=(None,))
+
+    V = embedding_matrix.shape[0]  # Vocabulary size
+    embeddings =   ks.layers.Embedding(output_dim=embedding_dim, input_dim=V,  
                         weights=embedding_matrix if embedding_matrix is None else [embedding_matrix],
                         mask_zero=True)(inputs)
 
-    h_outputs, h_last, c_last = ks.layers.LSTM(units=latent_dim, return_sequences=True, return_state=True)(embeddings)
+    # Application of the BiLSTM. We take the full sequence of hidden states `h_outputs`.
+    h_outputs, _, _, _, _ = ks.layers.Bidirectional(ks.layers.LSTM(units=latent_dim, return_sequences=True, 
+                                                        return_state=True))(embeddings)
 
+    # Final outputs of the model: for each input token, we produce a distribution for predicting its POS tag. 
+    # No activation function is used (`SparseCategoricalCrossentropy(from_logits=True)` is used as loss).
     outputs = ks.layers.Dense(units=n_classes)(h_outputs)
 
     model = ks.Model(inputs=inputs, outputs=outputs)
