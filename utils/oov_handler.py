478ad81a8a777e48d013c955daccef4a1a32b0f4
Enrico Pittini <pittinienrico@hotmail.it>
2022-11-30 21:07:35 +0100

    Update OOV handling

--- utils/oov_handler.py
+++ utils/oov_handler.py
@@ -1,10 +1,29 @@
+from collections import Counter
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from typing import List, Dict
 
-def get_co_occurrence_matrix(tokens: List[str], token2int: Dict[str, int], sentences: List[List[str]], window_size: int = 5) -> np.ndarray:
-    n_tokens = len(tokens)
 
+
+######### OOV WORDS ANALYSIS
+
+def get_OOV_analysis(embedding_model : Dict[str, np.array], texts: List[str]):
+    n_words = len(set([word for text in texts for word in text.split()]))
+    OOV_words_list = [word for text in texts for word in text.split() if word not in embedding_model]
+    OOV_words = set(OOV_words_list)
+    n_OOV_words = len(OOV_words)
+    proportion_OOV_words = n_OOV_words/n_words
+    OOV_words_counts = Counter(OOV_words_list)
+    OOV_words_counts = sorted(OOV_words_counts.items(), key=lambda x:x[1])[::-1]
+
+    return OOV_words, proportion_OOV_words, OOV_words_counts
+
+
+
+########## OOV WORDS TRAINING
+
+def _get_co_occurrence_matrix(n_tokens : int, token2int: Dict[str, int], sentences: List[List[str]], window_size: int = 5) -> np.ndarray:
     # Create the tokens co-occurence matrix filled with zeros
     co_occurrence_matrix = np.zeros(shape=(n_tokens, n_tokens), dtype=np.int32)
 
@@ -31,49 +50,28 @@ def _loss_function(weights, weights_t, bias, bias_t, co_occurence_matrix):
     loss = torch.sum(formula)
     return loss
 
-# number of training epochs
-#n_epochs = 4000
 
-# tolerance
-#eps = 0.001
-
-# number of sentences to consider
-#n_sents = 10
-
-# weight embedding size
-#embedding_size = 50
-
-# learning rate
-#alpha = 0.1
-
-# AdaGrad parameter 
-# delta = 0.8
-
-# top N similar words
-# topN = 5
-
-def train_oov_terms(GLOVE_embeddings, co_occurrence_matrix, 
-                    token2int, int2token, embedding_size = 50, n_epochs = 100):
+def _train_oov_terms(embedding_model, co_occurrence_matrix, token2int, int2token, n_epochs = 100, device='cpu'):
     n_tokens = len(token2int)
     
-    n_tokens = len(token2int)
+    embedding_size = list(embedding_model.values())[0].shape[0]
 
     # Variables declaration
-    knownIndices = np.array([token2int[key] if key in token2int.keys() else -1 for key in GLOVE_embeddings.keys()], dtype=np.int32)
-    knownEmbeddings = np.array([GLOVE_embeddings[int2token[k]] for k in knownIndices[knownIndices!=-1]], dtype=np.float32)
+    knownIndices = np.array([token2int[key] if key in token2int.keys() else -1 for key in embedding_model.keys()], dtype=np.int32)
+    knownEmbeddings = np.array([embedding_model[int2token[k]] for k in knownIndices[knownIndices!=-1]], dtype=np.float32)
 
-    weights = torch.randn((n_tokens, embedding_size), requires_grad=True, device='cuda')
-    bias = torch.randn((n_tokens, ), requires_grad=True, device='cuda')
-    weights_t = torch.randn((n_tokens, embedding_size), requires_grad=True, device='cuda')
-    bias_t = torch.randn((n_tokens, ), requires_grad=True, device='cuda')
+    weights = torch.randn((n_tokens, embedding_size), requires_grad=True, device=device)
+    bias = torch.randn((n_tokens, ), requires_grad=True, device=device)
+    weights_t = torch.randn((n_tokens, embedding_size), requires_grad=True, device=device)
+    bias_t = torch.randn((n_tokens, ), requires_grad=True, device=device)
 
-    co_occurrence_matrix=torch.tensor(co_occurrence_matrix, dtype=torch.int64, device='cuda')
+    co_occurrence_matrix=torch.tensor(co_occurrence_matrix, dtype=torch.int64, device=device)
 
-    mask=torch.ones((n_tokens, embedding_size), dtype=torch.int32, device='cuda')
-    k_embedding=torch.zeros((n_tokens, embedding_size), dtype=torch.float32, device='cuda')
+    mask=torch.ones((n_tokens, embedding_size), dtype=torch.int32, device=device)
+    k_embedding=torch.zeros((n_tokens, embedding_size), dtype=torch.float32, device=device)
 
-    knownIndices = torch.tensor(knownIndices, dtype=torch.long,  device='cuda')
-    knownEmbeddings = torch.tensor(knownEmbeddings, dtype=torch.float32,  device='cuda')
+    knownIndices = torch.tensor(knownIndices, dtype=torch.long,  device=device)
+    knownEmbeddings = torch.tensor(knownEmbeddings, dtype=torch.float32,  device=device)
 
     mask[knownIndices[knownIndices != -1]] = 0
     k_embedding[knownIndices[knownIndices != -1]] = knownEmbeddings.clone().detach()
@@ -108,3 +106,32 @@ def train_oov_terms(GLOVE_embeddings, co_occurrence_matrix,
         weights_T=mask*weights_t + (1-mask)* (k_embedding-weights)
 
     return (weights+weights_T).detach().cpu().numpy(), losses
+
+
+def extend_embedding_model(embedding_model : Dict[str, np.array], texts: List[str], window_size : int = 5,  n_epochs : int = 100, 
+                           device : str = 'cpu'):
+    texts_list = [sentence.lower().split(' ') for sentence in texts]
+    tokens = set([t for sentence in texts_list for t in sentence])
+    n_tokens = len(tokens)
+    token2int = dict(zip(tokens, range(len(tokens))))
+    int2token = {v: k for k,v in token2int.items()}
+
+    print('Building co-occurence matrix...')
+    co_occurrence_matrix = _get_co_occurrence_matrix(n_tokens, token2int, texts_list, window_size=window_size)
+    print('Co-occurence matrix shape:', co_occurrence_matrix.shape)
+    print()
+
+    print('Training OOV words...')
+    weights, losses = _train_oov_terms(embedding_model, co_occurrence_matrix, token2int, int2token, device=device, 
+                                       n_epochs=n_epochs)
+
+    plt.plot(losses)
+    plt.title('Training loss')
+    plt.xlabel('Epochs')
+    plt.grid()
+    plt.show()
+
+    extended_embedding_model = { t: weights[i] for i, t in enumerate(tokens) }
+    extended_embedding_model.update(embedding_model)
+
+    return extended_embedding_model
